b78303e44106e5dc232a60e823dd6605e4c4ddba
asifalam515 <asibulalam515@gmail.com>
2024-07-29 07:42:37 +0600

    stl

---
+++ stl/parirs.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  pair<int,int>p ={23,3};
+  cout<<p.first<<" "<<p.second;
+
+    return 0;
+}
---
+++ stl/vector.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+//   vectors are like array
+vector<int>v={2,23,64};
+v.push_back(23);
+for(auto it= v.begin();it < v.end();it++){
+    cout<<*it<<endl;
+
+}
+
+    return 0;
+}
